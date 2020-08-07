1ec9c40f8c903a826420b938c9f622ea967d1908
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-08-07 13:10:52 -0400

    mvp - working on stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,14 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon 
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,13 +24,23 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+  with open("cities.csv", newline='') as csvfile:
+      file = csv.reader(csvfile)
+      firstline = True
+      for row in file:
+          if firstline:
+              firstline = False
+              continue
+          newcity = City(row[0], float(row[3]), float(row[4]))
+          cities.append(newcity)
+  
+  return cities
 
 cityreader(cities)
 
@@ -63,9 +81,21 @@ for c in cities:
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    if lat1 >= lat2:
+      upperlat = lat1
+      upperlon = lon1
+      lowerlat = lat2
+      lowerlon = lon2
+    else:
+        upperlat = lat2
+        upperlon = lon2
+        lowerlat = lat1
+        lowerlon = lon2
 
-  return within
+    
+    within = []
+    for x in cities:
+      if (x.lat <= upperlat) and (x.lat >= lowerlat) and (x.lon <= upperlon) and (x.lon >= lowerlon):
+        within.append(x)
+
+    return within
